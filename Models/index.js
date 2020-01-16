16c16ddfa93d44da63b630afe97e1bd29bfd1c40
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2020-01-16 12:16:25 -0800

    add all color controller

--- Models/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-
-router.get('/', (req, res) => {
-    res.send('Here are all your colors!')
-})
-
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json({ limit: '10mb'}))
 
+app.use('/allColors', require('./controllers/allColors'))
 
-app.listen(process.env.PORT, () => {
-    console.log(`You are listening to the sweet sounds of ${process.env.PORT}`);
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`You are listening to the sweet sounds of server`);
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello there')
 })
 
 module.exports = app;
