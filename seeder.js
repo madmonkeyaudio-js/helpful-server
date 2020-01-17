3b613a35b982aed8e9ff3ce52a009cb28b4a70a1
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2020-01-16 16:53:45 -0800

    create seeder for color db

--- seeder.js
+++ seeder.js
@@ -1,8 +1,32 @@
+const db = require('./Models');
+
+let colorOptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+let myColor = []
+let allColors = [];
 
 
-let populateColors = () => {
+function findRandom(count) {
 
+    for(let i = 0; i < count; i++){
+        let randomDigit = colorOptions[Math.floor(Math.random() * colorOptions.length)];
+        myColor.push(randomDigit)
+    }
+    return myColor;
 }
 
+function populateColors(count) {
+    findRandom(6*128);
+    for(let i = 0; i < count; i++){
+        if(myColor.length){
+            let color = myColor.splice(0, 6)
+            allColors.push({
+                index: i,
+                hedId: color
+            })
+        }
+    }
 
-populateColors();
+    return allColors
+}
+
+console.log(populateColors(128))
