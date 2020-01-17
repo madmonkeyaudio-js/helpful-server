2244d41cec0c5182829ec6838dcd0a26206e578c
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2020-01-16 17:18:09 -0800

    client now has access to colors

--- controllers/allColors.js
+++ controllers/allColors.js
@@ -2,7 +2,13 @@ const router = require('express').Router();
 const db = require('../Models')
 
 router.get('/', (req, res) => {
-    res.send('Here are all your colors!')
+    db.Color.find()
+    .then(response => {
+        res.send(response)
+    })
+    .catch(err => {
+        console.log('Error locating colors', err)
+    })
 })
 
 module.exports = router;
