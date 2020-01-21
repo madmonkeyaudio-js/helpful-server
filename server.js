154eb847cca396df5907ea3e291dfeb301a5a782
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2020-01-21 12:34:03 -0800

    change port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: '10mb'}))
 
 app.use('/allColors', require('./controllers/allColors'))
 
-app.listen(process.env.PORT || 27017, () => {
+app.listen(process.env.PORT || 7066, () => {
     console.log(`You are listening to the sweet sounds of server`);
 })
 
