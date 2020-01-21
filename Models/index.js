16069f441c05ef8528c543d87fa05be7a9059561
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2020-01-21 11:32:12 -0800

    connect to mlab

--- Models/index.js
+++ Models/index.js
@@ -7,4 +7,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/helpfulHuman', 
     useCreateIndex: true
 })
 
+mongoose.connection.on('error', () => {
+    console.log('MongoDB Connection Error, please make sure MongoDB is running');
+    process.exit(1)
+})
+
 module.exports.Color = require('./color');
